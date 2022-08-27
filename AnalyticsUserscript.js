641857b7df1e96a15a4d364c745dd75e8d2607fa
Jorge Gámez <jgamezj@gmail.com>
2022-08-27 16:28:31 -0700

    userscript analytics
    userscript that changes interview titles and description on hover

---
+++ AnalyticsUserscript.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Trakken Interview
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Me
+// @match        https://www.analytics-summit.de/interviews/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let titles = document.querySelectorAll('.title');
+    console.log(titles);
+    titles.forEach((element, index) => {
+       const numberString = `${index}`.padStart(2, '0');
+       element.textContent = `EVENT - INTERVIEW - ${numberString}`;
+       const quote = element.nextElementSibling;
+       mouseHover(quote);
+       mouseHoverOut(quote);
+    });
+
+    function mouseHover(quote) {
+       quote.addEventListener("mouseover", (event) => {
+           quote.style.backgroundColor = "#666";
+           const childI = quote.querySelector('i');
+           childI.style.color = "white";
+       }, false);
+    }
+
+
+    function mouseHoverOut(quote) {
+       quote.addEventListener("mouseout", (event) => {
+           quote.style.backgroundColor = "";
+           const childI = quote.querySelector('i');
+           childI.style.color = "";
+       }, false);
+    }
+
+})();
